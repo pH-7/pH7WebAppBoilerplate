04c77d352ba57ce5c18a3e58c29593c4cad5d9f2
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-24 14:27:12 +0100

    Remove not used code

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -53,10 +53,3 @@ $app->error(function (Exception $except, Request $request, int $code) use ($app)
     return $app['twig']->render('error.html.twig', array('message' => $message));
 });
 
-// Register JSON data decoder for JSON requests
-$app->before(function (Request $request) {
-    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-        $data = json_decode($request->getContent(), true);
-        $request->request->replace(is_array($data) ? $data : array());
-    }
-});
