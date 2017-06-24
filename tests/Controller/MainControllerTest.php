1701cfc66ae62d1ab51e9ef776d0b40b2b780cd2
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-24 14:30:02 +0100

    Improve PHPUnit tests

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -7,18 +7,21 @@ namespace PH7\Boilerplate\Tests\Controller;
 use Mockery;
 use PH7\Boilerplate\Controller\MainController;
 use PHPUnit\Framework\TestCase;
-use Twig_Environment;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainControllerTest extends TestCase
 {
     public function testIndexAction(): void
     {
-        $mainController = Mockery::mock(MainController::class);
+        $request = Mockery::mock(Request::class)->makePartial();
+        $app = Mockery::mock(Application::class)->makePartial();
+        $mainController = Mockery::mock(MainController::class)->makePartial();
 
-        $this->assertInstanceOf(Twig_Environment::class, $mainController->indexAction());
+        // TODO Test it once your "MainController::indexAction()" is implemented
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
         /**
          * Reset the mock container to null. For integration with PHPUnit and others.
